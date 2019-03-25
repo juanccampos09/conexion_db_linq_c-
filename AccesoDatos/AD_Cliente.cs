22e1d2307490352e0fdcde734e25e23fceaf4ca3
Juan Carlos <juancalz53@gmail.com>
2019-03-24 22:41:55 -0600

    primer commit

---
+++ AccesoDatos/AD_Cliente.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace AccesoDatos
+{
+    public class AD_Cliente
+    {
+
+        public AD_Cliente()
+        {
+
+
+        }
+
+        public int guardar(Cliente pvo_cliente)
+        {
+
+            DB_ClientesDataContext vlo_dataContext = new DB_ClientesDataContext();
+            // variable necesaria para pasar por referencia el valor del id del cliente
+            int idCliente = pvo_cliente.Id; 
+
+            try
+            {
+                vlo_dataContext.SP_GUARDAR_CLIENTE(ref idCliente,
+                                                    pvo_cliente.Nombre,
+                                                    pvo_cliente.Telefono);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return idCliente;
+
+        }
+
+        public string eliminar(int pvn_idCliente)
+        {
+
+            DB_ClientesDataContext vlo_dataContext = new DB_ClientesDataContext();
+            string vlc_mensaje = null; // variable para obtener el mensaje del procedimiento almacenado
+
+            try
+            {
+
+                vlo_dataContext.SP_ELIMINAR_CLIENTE(pvn_idCliente, ref vlc_mensaje);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return vlc_mensaje;
+        }
+
+
+        public Cliente obtenerRegistro(int pvn_idCliente)
+        {
+
+            DB_ClientesDataContext vlo_dataContext = new DB_ClientesDataContext();
+            Cliente vlo_cliente = new Cliente();
+
+            try
+            {
+
+                var consulta = from /*CUALQUIER NOMBRE*/ cliente in vlo_dataContext.CLIENTE
+                               where cliente.ID == pvn_idCliente
+                               select cliente; // equivalente a decir "select * from cliente 
+                                                       // where id = @id"
+
+                // ciclo para obtener los datos del cliente
+                foreach (var item in consulta)
+                {
+                    vlo_cliente.Id = item.ID;
+                    vlo_cliente.Nombre = item.NOMBRE;
+                    vlo_cliente.Telefono = item.TELEFONO;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+            return vlo_cliente;
+
+        }
+
+        // cuando se listan los registros siempre es necesario que retorne un objeto
+        public object listaRegistros(string pvc_nombreCliente)
+        {
+
+            DB_ClientesDataContext vlo_dataContext = new DB_ClientesDataContext();
+
+            try
+            {
+                // verificando si se especifico el nombre del cliente
+                if (pvc_nombreCliente != null)
+                {
+
+                    var consulta = from /*CUALQUIER NOMBRE*/ cliente in vlo_dataContext.CLIENTE
+                                   where cliente.NOMBRE.Contains(pvc_nombreCliente) // indicando que muestre los registros que tienen un nombre como el del parametro
+                                   orderby cliente.ID // ordenandolos por id
+                                   select cliente;
+
+                    return consulta;
+
+                }
+                else
+                {
+
+                    var consulta = from cliente in vlo_dataContext.CLIENTE
+                                   orderby cliente.ID // ordenandolos por id
+                                   select cliente;
+
+                    return consulta;
+
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+    }
+}
---
+++ Logica/LogicaCliente.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using AccesoDatos;
+
+namespace Logica
+{
+    public class LogicaCliente
+    {
+
+        public LogicaCliente()
+        {
+
+
+        }
+
+        public int guardar(Cliente pvo_cliente)
+        {
+            AD_Cliente aD_Clientes = new AD_Cliente();
+            int idCliente = -1;
+
+            try
+            {
+                idCliente = aD_Clientes.guardar(pvo_cliente);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return idCliente;
+
+        }
+
+        public string eliminar(int pvn_idCliente)
+        {
+
+            string vlc_mensaje = null;
+            AD_Cliente aD_Cliente = new AD_Cliente();
+
+            try
+            {
+                vlc_mensaje = aD_Cliente.eliminar(pvn_idCliente);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return vlc_mensaje;
+
+        }
+
+        public Cliente obtenerRegistro(int pvn_idCliente)
+        {
+
+            Cliente vlo_cliente;
+            AD_Cliente aD_Cliente = new AD_Cliente();
+
+            try
+            {
+                vlo_cliente = aD_Cliente.obtenerRegistro(pvn_idCliente);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return vlo_cliente;
+
+        }
+
+        public object listaRegistros(string pvc_nombreCliente = null) // indicando que el paramentro puede ser nulo
+        {
+
+            AD_Cliente aD_Cliente = new AD_Cliente();
+            object consulta = null; // objeto que va a tener la lista de registros
+
+            try
+            {
+                consulta = aD_Cliente.listaRegistros(pvc_nombreCliente);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return consulta;
+        }
+
+    }
+}
